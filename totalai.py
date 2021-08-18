1b655f89230ae2f956bb9c769ad74b95b709686f
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-08-18 21:38:05 +0200

    Add basic functionality for single digit classification

---
+++ totalai.py
@@ -0,0 +1,28 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    args = parser.parse_args()
+
+    classifier = get_classifier()
+    predictions = predict(args.filename, classifier)
+
+    print(f"{len(predictions)} predictions found:")
+    print(*predictions)
+
+def get_classifier():
+    classifier = {}
+    with open("classifier.csv") as f:
+        for line in f:
+            items = line.strip().split(",")
+            image_class, *examples = items
+            classifier[image_class] = examples
+    return classifier
+
+def predict(image_name, classifier):
+    return [image_class for image_class, examples in classifier.items()
+            if any(example in image_name for example in examples)]
+
+if __name__ == "__main__":
+    main()
